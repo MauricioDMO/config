c415c2bf6a3a8cbb3898459b327ac008d14df436
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2026-02-26 17:55:15 -0600

    feat: Add xfce4-power-manager configuration and update setup script

--- deb/setup.zsh
+++ deb/setup.zsh
@@ -1,2 +1,3 @@
 ln -sfn ~/.config/config/deb/i3/config ~/.config/i3/config
 ln -sfn ~/.config/config/deb/i3status/config ~/.config/i3status/config
+ln -sfn ~/.config/config/deb/xfce4/xfce4-power-manager.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
