b370cb332fc65f9ab0d79b65fe3bd2e4ce203420
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2026-03-04 11:18:28 -0600

    feat: Add Synaptics touchpad configuration and update setup script for new config

--- deb/setup.zsh
+++ deb/setup.zsh
@@ -2,3 +2,4 @@ ln -sfn ~/.config/config/deb/i3/config ~/.config/i3/config
 ln -sfn ~/.config/config/deb/i3status/config ~/.config/i3status/config
 ln -sfn ~/.config/config/deb/xfce4/xfce4-power-manager.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
 ln -sfn ~/.config/config/deb/terminal/.p10k.zsh ~/.p10k.zsh
+ln -sfn ~/.config/config/deb/X11/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf
